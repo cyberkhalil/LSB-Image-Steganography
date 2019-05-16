3e48dd00b713a2bab5826aa5e097cac063f368c7
mahmoud khalil <khalil2535@protonmail.com>
2019-05-16 16:15:46 +0300

    implement hide text
    Signed-off-by: mahmoud khalil <khalil2535@protonmail.com>

--- Main.m
+++ Main.m
@@ -9,10 +9,9 @@ img = {};
 
 %% gui code
 % Main Figuare GUI
-figure("Position",[200 100 950 500],"MenuBar","none"...
-    ,"Resize","off");
+figure("Position",[200 100 950 500],"MenuBar","none","Resize","off");
 
-imgs_path_list = ["coloredChips.png","cameraman.tif"]; % TODO put new images directories here
+imgs_path_list = ["coloredChips.png","cameraman.tif"];
 imgs_list = ["coloredChips.png","cameraman.tif","Choose another image ..."];
 
 % Pop menu to choose image from
@@ -51,7 +50,10 @@ extract_button.String = "Extract Hidden Text";
 extract_button.Callback = @extract;
 
 jTextArea = javaObjectEDT('javax.swing.JTextArea');
-javacomponent(jTextArea,[15,220,200,200]);
+extract_txta = javacomponent(jTextArea,[15,220,200,200]);
+
+jLabel = javaObjectEDT('javax.swing.JLabel');
+extract_lbl = javacomponent(jLabel,[15,120,200,50]);
 
 % Hide GUI code
 jLabel = javaObjectEDT('javax.swing.JLabel',...
@@ -63,7 +65,10 @@ hide_button.String = "Hide Text";
 hide_button.Callback = @hide;
 
 jTextArea = javaObjectEDT('javax.swing.JTextArea');
-javacomponent(jTextArea,[770,220,175,200]);
+hide_txta = javacomponent(jTextArea,[770,220,175,200]);
+
+jLabel = javaObjectEDT('javax.swing.JLabel');
+hide_lbl = javacomponent(jLabel,[770,120,175,50]);
 
 %% Assigning img.Name,Path,Value,Type and showing the image..
     function assign_img(~,~)
@@ -123,51 +128,54 @@ javacomponent(jTextArea,[770,220,175,200]);
 assign_img(cimg);
 
 %% Extract function
-    function extract(head,~)
+    function extract(~,~)
         f_bin = dec2bin(img.Value);
         txt_bin = f_bin(:,8)';
         txt = binary_to_ascii(txt_bin);
-        try
-            jTextArea = javaObjectEDT('javax.swing.JTextArea',txt);
-            head.Parent.Children(7) = javacomponent(jTextArea,[15,220,200,200]);
-        catch
-            % maybe i missed something here.
+        if txt~="There is no Hidden Text in this image!"
+            extract_txta.setText(txt);
+            extract_lbl.setText('Done !');% TODO html
+        else
+            extract_lbl.setText('No Hidden Text found!');% TODO html
         end
     end
 %% Hide function
     function hide(~,~)
-        fig = figure("Name","HW1_Q3 : "+img.Name); % create a new figure
-        add_help_button(fig,"About HW1_Q3","Every image pixel contains 8-bit number ; the first image is the first bit for each pixel , and if the image is rgb one then it''s the first bit for the red ,green and blue 8-bit numbers  , then  the second image contains the 2''nd bit incrementally with the first one (contains 1''st & 2''nd) , and each image contains the next bit incrementally.");
-        
-        x = 2; % number of images on x axsis
-        y = 4; % number of images on y axsis
-        
+        hide_lbl.setText('Encoding Text ..');
+        txt = char(hide_txta.getText());
+        if length(txt)<1
+            hide_lbl.setText('Enter a Text to hide');
+            return;
+        end
         % create @shaping handle
         if(img.Type=="truecolor") % shaping for colored image
             r_unit_size = 1:img.Size;
             g_unit_size = 1+img.Size:2*img.Size;
             b_unit_size = 1+2*img.Size:3*img.Size;
             
-            shaping = @(f_dec) reshape([f_dec(r_unit_size),f_dec(g_unit_size),f_dec(b_unit_size)] ,img.Width,img.Height,3);
+            shaping = @(f_dec) ...
+                reshape([f_dec(r_unit_size)...
+                ,f_dec(g_unit_size),f_dec(b_unit_size)] ...
+                ,img.Width,img.Height,3);
         else    % shaping for not colored (gray) image
             shaping = @(f_dec)reshape(f_dec,img.Width,img.Height);
         end
         
         f_bin = dec2bin(img.Value);
-        f_bin_flipped = fliplr(f_bin);
-        
-        for i=1:8
-            subplot(x,y,i);
-            f_i_flipped = f_bin_flipped(:,1:i);
-            f_i = fliplr(f_i_flipped);
-            f_dec = bin2dec(f_i) * ((2^(9-i))-1);
-            
-            
-            f_final = shaping(f_dec);
-            
-            imshow(uint8(f_final));
-            title([num2str(i) '-bit']);
+        binary_txt = ascii_to_binary(txt);
+        if all(length(binary_txt)>size(f_bin))
+            hide_lbl.setText('The input text is too large !');
+            return;
         end
+        f_bin(1:length(binary_txt),8)=binary_txt;
+        clear binary_txt txt;
+        f_dec = bin2dec(f_bin);
+        f_final = uint8(shaping(f_dec));
+        imshow(f_final);
+        hide_lbl.setText('Writing image ..');
+        img.Value = f_final;
+        imwrite(f_final,img.Path);
+        hide_lbl.setText('Done !');
     end
 %% Utils
     function add_help_button(fig,title,content)
@@ -193,7 +201,7 @@ assign_img(cimg);
         if ascii_txt(1)==bin2dec('00000010')
             [~,i]=ismember(bin2dec('00000011'),ascii_txt,'R2012a');
             if i~=0
-                ascii_txt = ascii_txt(1:i);
+                ascii_txt = ascii_txt(2:i-1);
                 return;
             end
         end
@@ -201,8 +209,10 @@ assign_img(cimg);
     end
     function [binary_txt] = ascii_to_binary(ascii_txt)
         % TODO check this
-        binary_txt(1) = '00000010';
-        binary_txt(2:1+length(ascii_txt)*8) = char(dec2bin(ascii_txt));
-        binary_txt(end) = '00000011';
+        binary_txt = '00000010';
+        binary_txt(2:1+length(ascii_txt),:) = '0';
+        binary_txt(2:1+length(ascii_txt),2:8) = char(dec2bin(ascii_txt));
+        binary_txt(end+1,:) = '00000011';
+        binary_txt = reshape(binary_txt,1,[]);
     end
 end
